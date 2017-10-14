301698ae2099ea781b5d98df411ffc26750c7565
jruszkowski <joseph.ruszkowski@gmail.com>
2017-10-14 11:54:16 +0000

    faster fd

--- run_5.py
+++ run_5.py
@@ -25,12 +25,8 @@ for i in startindex:
 plyr_dict['Todd Gurley II'] = plyr_dict.pop('Todd Gurley')
 d_plyr_dict = {x.split(' ')[0]: y for (x,y) in plyr_dict.items() if x.split(' ')[1] == 'D/ST'}
 
-df = pd.read_csv('fanduel_9.csv').set_index('Nickname')
+df = pd.read_csv('fanduel.csv').set_index('Nickname')
 df['Projection'] = pd.DataFrame.from_dict(plyr_dict, orient='index')
-df = df.reset_index()
-df.set_index('Last Name', inplace=True)
-df.loc[df['Position']=='D', 'Projection'] = pd.DataFrame.from_dict(d_plyr_dict, orient='index')[0]
-df = df.reset_index().set_index('Nickname')
 df = df[df['Injury Indicator'].isnull()]
 df[df['Projection'].isnull()]
 df = df[df['Projection'] > 1]
@@ -38,11 +34,7 @@ df = df[df['Projection'] > 1]
 min_salary = df.groupby(['Position'])['Salary'].agg([np.min])['amin'].to_dict()
 min_rb_projection = df[df['Salary'] == min_salary['RB']][df['Position'] == 'RB']['Projection'].max()
 min_wr_projection = df[df['Salary'] == min_salary['WR']][df['Position'] == 'WR']['Projection'].max()
-min_k_projection = df[df['Salary'] == min_salary['K']][df['Position'] == 'K']['Projection'].max()
-min_te_projection = df[df['Salary'] == min_salary['TE']][df['Position'] == 'TE']['Projection'].max()
-min_d_projection = df[df['Salary'] == min_salary['D']][df['Position'] == 'D']['Projection'].max()
-min_dict = {'QB': 1, 'D': 1, 'RB': min_rb_projection, 'WR': min_wr_projection,\
-         'K': min_k_projection, 'TE': min_te_projection, 'D': min_d_projection}
+min_dict = {'QB': 1, 'RB': min_rb_projection, 'WR': min_wr_projection}
 
 
 grouped = df.groupby(['Position'])
@@ -52,25 +44,18 @@ for pos, frame in grouped:
 
 
 def total_lineup(combo, key):
-    return round(position_dict['QB'][combo[0]][key] + \
-        position_dict['TE'][combo[1]][key] + \
-        position_dict['K'][combo[3]][key] + \
-        position_dict['D'][combo[2]][key], 2)
+    return round(position_dict['QB'][combo[0]][key], 2)
 
 def total_lineup_all(combo, key):
-	other = combo[0]
+	qb = combo[0]
 	rb = combo[1]
 	wr = combo[2]
 
-	return round(position_dict['QB'][other[0]][key] + \
-		position_dict['TE'][other[1]][key] + \
-		position_dict['K'][other[3]][key] + \
-		position_dict['D'][other[2]][key] + \
+	return round(position_dict['QB'][qb][key] + \
 		position_dict['RB'][rb[0]][key] + \
 		position_dict['RB'][rb[1]][key] + \
 		position_dict['WR'][wr[0]][key] + \
-		position_dict['WR'][wr[1]][key] + \
-		position_dict['WR'][wr[2]][key], 2)
+		position_dict['WR'][wr[1]][key], 2)
 
 
 def add_func(position, plyrs, key):
@@ -84,14 +69,10 @@ def create_salary_dict():
 	return {salary: {'players': [], 'projection': 0} for salary in range(0,60100,100)}
 
 
-combos = {'RB': 2, 'WR': 3, 'QB': 1}
+combos = {'RB': 2, 'WR': 2, 'QB': 1}
 rb_dict = create_salary_dict()
 wr_dict = create_salary_dict()
 qb_dict = create_salary_dict()
-singles_list = ((qb, te, d, k) for qb in position_dict['QB'].keys() \
-				for te in position_dict['TE'].keys() \
-				for d in position_dict['D'].keys() \
-				for k in position_dict['K'])
 
 
 def create_combo_dictionaries(combo_args):
@@ -112,12 +93,13 @@ def create_combo_dictionaries(combo_args):
 				wr_dict[salary]['projection'] = projection
 				wr_dict[salary]['players'] = combo 
 	elif position == 'QB': 
-		for combo in singles_list:
-			projection = total_lineup(combo, 'Projection')
-			salary = total_lineup(combo, 'Salary')
+		for qb in position_dict['QB'].keys():
+			print (qb, position_dict.keys())
+			projection = position_dict['QB'][qb]['Projection']
+			salary = position_dict['QB'][qb]['Salary']
 			if projection > qb_dict[salary]['projection']:
 				qb_dict[salary]['projection'] = projection
-				qb_dict[salary]['players'] = combo 
+				qb_dict[salary]['players'] = qb 
 
 def clean_dict(dict_zeros):
 	for key in dict_zeros.keys():
@@ -145,5 +127,10 @@ if __name__=="__main__":
 			rb_dict[rb]['players'], wr_dict[wr]['players']), 'Salary') <= 60000}
 
 	max_projection = max([total_dict[x]['projection'] for x in total_dict.keys()])
-        df = pd.DataFrame.from_dict(total_dict, orient='index').reset_index().set_index('projection').sort(ascending=False)
-        print (df.head(10))
+	df = pd.DataFrame.from_dict(total_dict, orient='index').reset_index().set_index('projection').sort(ascending=False)
+	print (df.head(15))
+	#for key in total_dict.keys():
+	#	print total_dict[key]['projection'], key
+	#	if total_dict[key]['projection'] == max_projection:
+	#		print key
+	print (datetime.datetime.now() - start_time)
